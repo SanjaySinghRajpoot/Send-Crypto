fcc8ae2d0765e421daa48ef20f1b3b3db7cee8dd
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2022-04-09 09:53:29 +0530

    files

--- src/App.js
+++ src/App.js
@@ -1,48 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
-import Web3 from 'web3';
-import { useEffect, useState } from 'react';
-import detectEthereumProvider from '@metamask/detect-provider';
-import { loadContract } from "./Utils/loadContract "
+
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import Web3 from "web3";
+import detectEthereumProvider from '@metamask/detect-provider'
+import { loadContract } from "./utils/load-contract";
+
 
 function App() {
   const [web3Api, setWeb3Api] = useState({
     provider: null,
+    isProviderLoaded: false,
     web3: null,
-    contract: null,
+    contract: null
   })
 
-  const [accounts, setAccounts] = useState(null);
- 
+  const [balance, setBallance] = useState(null)
+  const [account, setAccount] = useState(null)
+  const [shouldReload, reload] = useState(false)
+
+  const canConnectToContract = account && web3Api.contract
+  const reloadEffect = useCallback(() => reload(!shouldReload), [shouldReload])
+
+  const setAccountListener = provider => {
+    provider.on("accountsChanged", _ => window.location.reload())
+    provider.on("chainChanged", _ => window.location.reload())
+  }
+
   useEffect(() => {
     const loadProvider = async () => {
-
       const provider = await detectEthereumProvider()
-      const { contract } = await loadContract("Faucet", provider)
 
-      if(provider){
-        provider.request({method: "eth_requestAccounts"})
+      if (provider) {
+        const contract = await loadContract("Faucet", provider)
+        setAccountListener(provider)
         setWeb3Api({
-          web3: new web3(provider),
-          provider: provider,
-          contract: contract,
+          web3: new Web3(provider),
+          provider,
+          contract,
+          isProviderLoaded: true
         })
       } else {
-        console.error("Please intall metamask");
+        setWeb3Api(api => ({...api, isProviderLoaded: true}))
+        console.error("Please, install Metamask.")
       }
-
-      loadProvider()
     }
+
+    loadProvider()
   }, [])
 
   useEffect(() => {
-    const getAccount  = async() => {
-        const accounts = await web3Api.web3.eth.getAccounts()
-        setAccounts(accounts[0]);   // select the first account from the array
-    } 
+    const loadBalance = async () => {
+      const { contract, web3 } = web3Api
+      const balance = await web3.eth.getBalance(contract.address)
+      setBallance(web3.utils.fromWei(balance, "ether"))
+    }
 
-    web3Api.web3 && getAccount()  // only check for account if web3 account exist 
-  },[web3Api.web3])
+    web3Api.contract && loadBalance()
+  }, [web3Api, shouldReload])
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts()
+      setAccount(accounts[0])
+    }
+
+    web3Api.web3 && getAccount()
+  }, [web3Api.web3])
+
+  const addFunds = useCallback(async () => {
+    const { contract, web3 } = web3Api
+    await contract.addFunds({
+      from: account,
+      value: web3.utils.toWei("1", "ether")
+    })
+
+    reloadEffect()
+  }, [web3Api, account, reloadEffect])
+
+  const withdraw = async () => {
+    const { contract, web3 } = web3Api
+    const withdrawAmount = web3.utils.toWei("0.1", "ether")
+    await contract.withdraw(withdrawAmount, {
+      from: account
+    })
+    reloadEffect()
+  }
 
   return (
     <>
@@ -104,3 +146,13 @@ function App() {
 }
 
 export default App;
+
+
+// Private key 32 byte number
+// c0ab562fa567abc1597a9f9c840537342809a387f6d45f5e112d0d074c6875ce
+
+// Public key(Uncompressed) 64 byte number
+// 048bd5fbf4bc3d8421b8024229943170babd858b9338552ddccb2fa3da24f867ca071f658662bc263ef3272e15fd10a3abc9533991586f2e93136f548db9cb921f
+
+// Public key(Compressed) 33 byte number
+// 038bd5fbf4bc3d8421b8024229943170babd858b9338552ddccb2fa3da24f867ca
